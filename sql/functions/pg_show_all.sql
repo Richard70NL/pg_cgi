f21a453c4e1195631c38255062bbf83e77c2098b
Richard70NL <richard@richard70.nl>
2019-03-27 20:41:06 +0100

    Update pg_show_all.sql

--- sql/functions/pg_show_all.sql
+++ sql/functions/pg_show_all.sql
@@ -1,5 +1,6 @@
 create or replace function cgi.pg_show_all() returns void as $$
 declare
+  rec record;
 begin
   -- Tell the cgi app that the response needs to be extracted from the
   -- cgi_response_text table.
@@ -12,9 +13,15 @@ begin
   -- Create Content-type header
   perform cgi.set_content_type('text/plain');
 
-  -- Output some text content.
+  -- Output t_cgi_param table content
   perform cgi.println('t_cgi_param table content:');
   perform cgi.println('--------------------------');
   perform cgi.new_line();
+
+  for rec in select f_type, f_name, f_index, f_value
+    from t_cgi_param order by f_type, f_name, f_index
+  loop
+    perform cgi.println(rec.f_type || ', ' || rec.f_name || '[' || rec.f_index || ']: ' || rec.f_value);
+  end loop;
 end;
 $$ language plpgsql;
