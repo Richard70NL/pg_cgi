f6f546f429b92e2c4f13b441bdd1b8f8f733d3d8
Richard70NL <richard@richard70.nl>
2019-03-28 13:43:25 +0100

    Update pg_show_all.sql

--- sql/functions/pg_show_all.sql
+++ sql/functions/pg_show_all.sql
@@ -1,17 +1,18 @@
-create or replace function cgi.pg_show_all() returns void as $$
+create or replace function cgi.pg_show_all(v_no_headers bool default false) returns void as $$
 declare
   rec record;
 begin
-  -- Tell the cgi app that the response needs to be extracted from the
-  -- cgi_response_text table.
-  insert into t_cgi_param(f_type, f_name, f_index, f_value)
-    values('response', 'output', 0, 'text');
+  if not v_no_headers then
+    -- Tell the cgi app that the response needs to be extracted from the
+    -- cgi_response_text table.
+    perform cgi.set_cgi_param('response', 'output', 'text');
 
-  -- Tell the cgi app what the HTTP status line is.
-  perform cgi.set_http_status(200, 'OK');
+    -- Tell the cgi app what the HTTP status line is.
+    perform cgi.set_http_status(200, 'OK');
 
-  -- Create Content-type header
-  perform cgi.set_content_type('text/plain');
+    -- Create Content-type header
+    perform cgi.set_content_type('text/plain');
+  end if;
 
   -- Output t_cgi_param table content
   perform cgi.println('t_cgi_param table content:');
